c91c91c765095c13527d7e686dfd9d8f0cc5634c
Harrison Wang <harrisonwang@163.com>
2025-03-11 08:46:33 +0800

    init

---
+++ src/main/java/com/example/benefly/BeneflyApplication.java
@@ -0,0 +1,13 @@
+package com.example.benefly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeneflyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeneflyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/benefly/BeneflyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.benefly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeneflyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
